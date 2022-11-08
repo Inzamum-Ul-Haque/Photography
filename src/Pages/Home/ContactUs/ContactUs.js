73863979e5848e02c8274580b3f3db0efa2de3db
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2022-11-09 00:58:50 +0600

    review and contact design done

--- src/Pages/Home/ContactUs/ContactUs.js
+++ src/Pages/Home/ContactUs/ContactUs.js
@@ -1,7 +1,84 @@
 import React from "react";
+import { GrLocation } from "react-icons/gr";
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { AiOutlineMail } from "react-icons/ai";
+import "./ContactUs.css";
 
 const ContactUs = () => {
-  return <div>Contact section</div>;
+  return (
+    <div className="contact-container max-w-screen-xl mx-auto mb-16">
+      <div className="contact-header text-center mb-10">
+        <h2 className="text-5xl text-center mb-5 font-bold">Contact Us</h2>
+        <p className="text-2xl">
+          For hiring services or to give a review or any kind of questions,
+          please contact us or message us.
+        </p>
+      </div>
+      <div className="grid lg:grid-cols-2 sm:grid-cols-1 gap-10 contact-body">
+        <div>
+          <div className="flex mb-16">
+            <div className="bg-slate-400 rounded-full w-14 flex justify-center items-center mr-5 shrink-0">
+              <GrLocation />
+            </div>
+            <div className="text-left">
+              <h3 className="text-2xl font-bold">Address</h3>
+              <p className="text-base font-semibold">
+                Road-03, House-10, Block-A, Mirpur-11, 1216
+              </p>
+            </div>
+          </div>
+          <div className="flex mb-16">
+            <div className="bg-slate-400 rounded-full text-black w-14 flex justify-center items-center mr-5 shrink-0">
+              <BsFillTelephoneFill />
+            </div>
+            <div className="text-left">
+              <h3 className="text-2xl font-bold">Phone</h3>
+              <p className="text-base font-semibold">+8801750414709, 8012489</p>
+            </div>
+          </div>
+          <div className="flex mb-16">
+            <div className="bg-slate-400 rounded-full text-black w-14 flex justify-center items-center mr-5 shrink-0">
+              <AiOutlineMail />
+            </div>
+            <div className="text-left">
+              <h3 className="text-2xl font-bold">Email</h3>
+              <p className="text-base font-semibold">
+                photographer.service@gmail.com
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="card w-full bg-white">
+          <h1 className="text-4xl font-bold">Send Message</h1>
+          <div className="card-body">
+            <div className="form-control">
+              <input
+                type="text"
+                placeholder="Full name"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <input
+                type="text"
+                placeholder="Email"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <textarea
+                className="textarea textarea-bordered"
+                placeholder="Your message"
+              ></textarea>
+            </div>
+            <div className="form-control mt-6 w-1/4">
+              <button className="btn btn-primary">Send</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ContactUs;
--- src/Pages/Home/Reviews/Reviews.js
+++ src/Pages/Home/Reviews/Reviews.js
@@ -1,7 +1,86 @@
 import React from "react";
+import "./Reviews.css";
+import { FaStar, FaStarHalfAlt, FaRegStar } from "react-icons/fa";
+import client1 from "../../../Assets/images/clients/img-1.jpg";
+import client2 from "../../../Assets/images/clients/img-2.jpg";
+import client3 from "../../../Assets/images/clients/img-3.jpg";
 
 const Reviews = () => {
-  return <div>This will be reviews section</div>;
+  return (
+    <div className="reviews-container max-w-screen-xl mx-auto mb-16">
+      <h1 className="text-5xl text-center mb-10 services-header font-bold">
+        Reviews from Clients
+      </h1>
+      <div className="grid justify-items-center xl:grid-cols-3 xl:gap-8 lg:grid-cols-2 lg:gap-8 md:grid-cols-2 md:gap-8 sm:grid-cols-1 gap-8 sm:gap-4">
+        <div className="bg-stone-300 p-6 review-card rounded-lg shadow-2xl">
+          <div className="avatar">
+            <div className="w-24 rounded-full">
+              <img src={client1} alt="" />
+            </div>
+          </div>
+          <h4 className="text-2xl font-bold mb-5 mt-2">Fahmeed Mahmud</h4>
+          <div className="flex justify-center text-2xl mb-5">
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaStar />
+          </div>
+          <p className="text-lg">
+            The service provided by the photographer is really very good. They
+            are quick with response and very warm to the service. The clicks
+            were amazing and the work was really like a professional. You really
+            need to have a proper photography knowledge to do these kinds of
+            work.
+          </p>
+        </div>
+        <div className="bg-stone-300 p-6 review-card rounded-lg shadow-2xl">
+          <div className="avatar">
+            <div className="w-24 rounded-full">
+              <img src={client2} alt="" />
+            </div>
+          </div>
+          <h4 className="text-2xl font-bold mb-5 mt-2">Rafsan Habib</h4>
+          <div className="flex justify-center text-2xl mb-5">
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaRegStar />
+          </div>
+          <p className="text-lg">
+            কাজ বেশ ভালো ছিল। তারা তাদের কাজের ব্যাপারে খুবই দক্ষ এবং আমার কাছে
+            তাদের কাজ ভালো লেগেছে, এমনকি কন্টাক্ট থেকে শুরু করে ফাইনাল সার্ভিস
+            দেওয়া পর্যন্ত তারা অনেক আন্তরিক ছিল। আউট অফ ১০ আমি ৭ জনকে সাজেস্ট
+            করবো এই সার্ভিস টি নেওার জন্য। তাদের জন্য শুভ কামনা রইলো।
+          </p>
+        </div>
+        <div className="bg-stone-300 p-6 review-card rounded-lg shadow-2xl">
+          <div className="avatar">
+            <div className="w-24 rounded-full">
+              <img src={client3} alt="" />
+            </div>
+          </div>
+          <h4 className="text-2xl font-bold mb-5 mt-2">Abrar Rafid Noor</h4>
+          <div className="flex justify-center text-2xl mb-5">
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaStarHalfAlt />
+            <FaRegStar />
+          </div>
+          <p className="text-lg">
+            It was a nice experience to have them on the occasion of our
+            wedding. They worked hard and tried to give a satisfactory result.
+            Excellent equipments also enhanced the quality of work. Editing was
+            up to the mark, but there were some points in editing that looked a
+            bit awkward and bizzarred to me that shouldn't been in the first
+            place. I wish them all the best in their future endeavors.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Reviews;
